7a76c0e8097472d6e28ea911e6b30a24cdd0fe54
Nic Jansma <nic@nicj.net>
2020-04-26 22:11:01 -0400

    Misc changes

--- raspi_skimscan.py
+++ raspi_skimscan.py
@@ -18,13 +18,9 @@ from PIL import ImageFont
 
 
 # Raspberry Pi pin configuration:
-RST = 24     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+RST = None # I2C
 
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 disp.begin()
 
 time.sleep(3)
@@ -39,36 +35,40 @@ draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 ellipsis = ".   "
 phase = 0
+found = 0
 
 while True:
-        draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        draw.text((0, 24), "scanning" + ellipsis, font=font, fill=255)
-        disp.image(image)
-        disp.display()
+    print("scanning" + ellipsis)
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    draw.text((0, 24), "scanning" + ellipsis, font=font, fill=255)
+    draw.text((0, 36), str(found) + " BT found", font=font, fill=255)
+    disp.image(image)
+    disp.display()
 
-	nearby_devices = bluetooth.discover_devices(duration=10, lookup_names=True)
+    nearby_devices = bluetooth.discover_devices(duration=10, lookup_names=True)
 
-	print("found %d devices" % len(nearby_devices))
+    found = len(nearby_devices)
+    print("found %d devices" % found)
 
-	for addr, name in nearby_devices:
-		if (name == "HC-05") or (name == "HC-03") or (name == "HC-06"):
-			draw.rectangle((0, 0, width, height), outline=0, fill=0)
-			draw.text((0, 12), "Potential skimmer", font=font, fill=255)
-			draw.text((0, 24), name + " found.", font=font, fill=255)
-			draw.text((0, 36), "Skip this pump.", font=font, fill=255)
+    for addr, name in nearby_devices:
+        if (name == "HC-05") or (name == "HC-03") or (name == "HC-06"):
+            draw.rectangle((0, 0, width, height), outline=0, fill=0)
+            draw.text((0, 12), "Potential skimmer", font=font, fill=255)
+            draw.text((0, 24), name + " found.", font=font, fill=255)
+            draw.text((0, 36), "Skip this pump.", font=font, fill=255)
 
-			disp.image(image)
-			disp.display()
-			time.sleep(5)
+            disp.image(image)
+            disp.display()
+            time.sleep(5)
 
-	phase += 1
-	if phase == 1:
-		ellipsis = "..  "
-	elif phase == 2:
-		ellipsis = "... "
-	elif phase == 3:
-		ellipsis = "...."
-	else:
-		ellipsis = ".   "
-		phase = 0
+    phase += 1
+    if phase == 1:
+        ellipsis = "..  "
+    elif phase == 2:
+        ellipsis = "... "
+    elif phase == 3:
+        ellipsis = "...."
+    else:
+        ellipsis = ".   "
+        phase = 0
 
